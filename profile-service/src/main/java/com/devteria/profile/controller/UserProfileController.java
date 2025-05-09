d29edff59db87b2b7c216ad34e9bb0b6f56fa634
trinhquangtoan2112 <trinhtoanquang@gmail.com>
2025-05-09 08:52:51 +0700

    .

--- profile-service/src/main/java/com/devteria/profile/controller/UserProfileController.java
+++ profile-service/src/main/java/com/devteria/profile/controller/UserProfileController.java
@@ -1,5 +1,10 @@
 package com.devteria.profile.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.devteria.profile.dto.request.ProfileCreationRequest;
 import com.devteria.profile.dto.response.UserProfileResponse;
@@ -8,11 +13,6 @@ import com.devteria.profile.service.UserProfileService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
