ea37975d370c0889ce36b0e16d75e0fd8c0ae955
trinhquangtoan2112 <trinhtoanquang@gmail.com>
2025-05-13 09:14:56 +0700

    .

--- profile-service/src/main/java/com/devteria/profile/service/UserProfileService.java
+++ profile-service/src/main/java/com/devteria/profile/service/UserProfileService.java
@@ -1,5 +1,7 @@
 package com.devteria.profile.service;
 
+import java.util.List;
+
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +15,6 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
